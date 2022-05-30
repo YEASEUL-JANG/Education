d90429cf92741048ebe4edc037d370fcae373774
Yeaseul Jang <changys1212@naver.com>
2022-05-30 18:56:07 +0900

    commit

---
+++ java계정.sql
@@ -0,0 +1,22 @@
+show parameter undo;
+alter system set undo_retention=1500;
+
+select * from tab;
+select * from member;
+delete from member 
+where userid='kim';
+
+-- 삭제된 레코드 확인
+select*from member as of
+TIMESTAMP(systimestamp-interval '15'minute)
+where userid='kim';
+
+--삭제된 레코드 복구
+insert into member select*from member as of
+TIMESTAMP(systimestamp-interval '15'minute)
+where userid='kim';
+
+select * from member;
+
+
+
